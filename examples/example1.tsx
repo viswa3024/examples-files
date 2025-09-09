07a81ab5a7331756074eb778b7d64ab413a09303
K Kasi Viswanath <95619173+viswa3024@users.noreply.github.com>
2025-09-09 11:27:22 +0530

    Create example1.tsx

---
+++ examples/example1.tsx
@@ -0,0 +1,10 @@
+ <div className="relative w-[200px] h-[150px] overflow-hidden rounded group">
+  <img
+    className="w-full h-full object-cover rounded transform-gpu transition-transform duration-300 group-hover:scale-[1.02] group-hover:brightness-102"
+    style={{
+      transformOrigin: 'center',
+      willChange: 'transform',
+      backfaceVisibility: 'hidden',
+    }}
+  />
+</div>
